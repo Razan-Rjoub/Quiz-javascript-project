f4a8e558dcc9d8fef0d6d2815935f164c8fc9262
Razan-Rjoub <ralrjoub6@gmail.com>
2023-07-25 12:22:28 +0300

    Co-authored-by: Razan-Mustafa <Razan-Mustafa@users.noreply.github.com> Co-authored-by: Yousef-M-Jaradat <Yousef-M-Jaradat@users.noreply.github.com>

--- Questions/Questions.js
+++ Questions/Questions.js
@@ -1,4 +1,3 @@
-
 var numcount = 0;
 
 const http = new XMLHttpRequest();
@@ -10,40 +9,45 @@ function loadQuestion(index) {
 
         var divcontainer = document.getElementsByClassName("container")[0];
 
-        var question = `<h2 class="qustiontext">${++numcount}- ${questions[index].Q}</h2>
-        <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> A </span> <span class="Question">${questions[index].awr1}</span> </button></div>
-        <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> B </span> <span class="Question">${questions[index].awr2}</span> </button></div>
-        <div  onclick="ClickBtnChoice()" class="Choices"><button class="btncoice" ><span class="Letter"> C </span> <span class="Question">${questions[index].awr3}</span> </button></div>
-      
-        `
+        var question = `<h2 class="qustiontext">${++numcount}- ${questions[index].Q
+            }</h2>
+        <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> A </span> <span class="Question">${questions[index].awr1
+            }</span> </button></div>
+        <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> B </span> <span class="Question">${questions[index].awr2
+            }</span> </button></div>
+        <div  onclick="ClickBtnChoice()" class="Choices"><button class="btncoice" ><span class="Letter"> C </span> <span class="Question">${questions[index].awr3
+            }</span> </button></div>
+        `;
         divcontainer.innerHTML = question;
-        next.innerHTML = ''
-        next.style.display = 'none'
-
+        next.innerHTML = "";
+        next.style.display = "none";
     };
     http.send();
 }
 function session() {
-    var getsession = sessionStorage.getItem('index');
-    var question = `<h2 class="qustiontext">${++numcount}- ${questions[getsession].Q}</h2>
-    <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> A </span> <span class="Question">${questions[getsession].awr1}</span> </button></div>
-    <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> B </span> <span class="Question">${questions[getsession].awr2}</span> </button></div>
-    <div  onclick="ClickBtnChoice()" class="Choices"><button class="btncoice" ><span class="Letter"> C </span> <span class="Question">${questions[getsession].awr3}</span> </button></div>
-    `
+    var getsession = sessionStorage.getItem("index");
+    var question = `<h2 class="qustiontext">${++numcount}- ${questions[getsession].Q
+        }</h2>
+    <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> A </span> <span class="Question">${questions[getsession].awr1
+        }</span> </button></div>
+    <div onclick="ClickBtnChoice()" class="Choices"><button class="btncoice"  ><span class="Letter"> B </span> <span class="Question">${questions[getsession].awr2
+        }</span> </button></div>
+    <div  onclick="ClickBtnChoice()" class="Choices"><button class="btncoice" ><span class="Letter"> C </span> <span class="Question">${questions[getsession].awr3
+        }</span> </button></div>
+    `;
     divcontainer.innerHTML = question;
 }
 
-var next = document.getElementsByClassName('nextbtn')[0];
+var next = document.getElementsByClassName("nextbtn")[0];
 loadQuestion(0);
 var index = 0;
 function ClickBtnChoice() {
-    next.style.display = 'inline'
-    next.innerHTML = 'next'
-    console.log(next.innerHTML)
+    next.style.display = "inline";
+    next.innerHTML = "next";
+    console.log(next.innerHTML);
 }
 function nextqus() {
     ++index;
-    sessionStorage.setItem('index', index);
+    sessionStorage.setItem("index", index);
     loadQuestion(index);
-
 }
